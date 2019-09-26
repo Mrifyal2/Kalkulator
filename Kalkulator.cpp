b2aac70bc88af79184fcb2f693a336be12e8b368
Mrifyal2 <55224648+Mrifyal2@users.noreply.github.com>
2019-09-26 17:38:48 +0800

    Add files via upload

---
+++ Kalkulator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y, z, hasil;
+	
+	cout << "Menu : " << endl;
+	cout << "1. Perkalian (*) "<< endl;
+	cout << "2. pembagian (:) " << endl;
+	cout << "3. pengurangan (-) " << endl;
+	cout << "4. Pertambahan (+) " << endl;
+	cout << " " << endl;
+	
+	cout <<"Pilihan Operator : ";
+	cin >> z;
+	
+	cout << "Masukkan Nilai Pertama : ";
+	cin >> x;
+	
+	cout << "Masukkan Nilai Kedua : ";
+	cin >> y;
+	
+	if (z==1) {
+		hasil = x*y;
+		cout << "Hasil Perkalian : " << hasil;
+	} else if (z==2) {
+		hasil = x/y;
+		cout << "Hasil Pembagian :" << hasil;
+	} else if (z==3) {
+		hasil = x-y;
+		cout << "Hasil Pengurangan :" << hasil;
+	} else if (z==4) {
+		hasil = x+y;
+		cout << "Hasil Pertambahan :" << hasil;
+	}
+	return 0;
+}
